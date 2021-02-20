657d90d400a7ed6367b183835bbed447a67d71c4
Abhilash2015mca <abhilashjohn64@gmail.com>
2021-02-20 23:48:21 +0530

    21feb

---
+++ JS/start.js
@@ -0,0 +1,12 @@
+let person = {
+    name:'Abhi',
+    age: '12',
+       };
+console.log(person)
+person.age = 2;
+console.log(person)
+let selection = 'age';
+person[selection] = 1;
+console.log(person);
+let normandy = ['a','b',1];
+console.log(normandy)
