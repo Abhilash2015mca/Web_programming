15f5ebc774c9b5efea3efbd7ca71b0c94e444223
Abhilash <abhilashjohn64@gmail.com>
2021-07-02 13:07:25 +0530

    Lab Exam

--- Lab Exam/convention/user.php
+++ Lab Exam/convention/user.php
@@ -1,19 +1,26 @@
 <?php 
 
 require 'dbConnect.php';
-// echo "<h2 align='center' style='color:#008888;'> Book Convention Centers</h2>";
-// echo '<form action="#">';
-// echo 'Enter the date<input type="text" name="date">';
-// echo '<td> <input type="submit" value="Submit"> </td>';
-// echo "</form>";
-// $date = $_POST['date'];
-// $query = "SELECT * FROM centers where date='".$date."'";
-$query = "SELECT * FROM centers";
+echo "<h2 align='center' style='color:#008888;'> Book Convention Centers</h2>";
+echo '<form action="#">';
+echo 'Enter the date<input type="text" name="date">';
+echo '<td> <input type="submit" value="Submit"> </td>';
+echo "</form>";
+$date = $_POST['date'];
+$query = "SELECT * FROM centers where date='".$date."'";
 $result = mysqli_query($con,$query);
 
 
 if(mysqli_num_rows($result)>0)
 {
+    echo "<h2 align='center' style='color:#008888;'> User Information</h2>";
+    echo "<form action='#' method='post'>";
+    echo "Name <input type='text' name='name'><br><br>";
+    echo "Contact details<input type='number' name='contact'><br><br>";
+    echo "Occasion date<input type='date' name='occasion'><br><br>";
+    echo "Capacity<input type='number' name='capacity'><br><br>";
+    echo "</form>";
+
     echo "<h2 align='center' style='color:#008888;'> Available Convention Centers</h2>";
     echo "<table border='1' align='center' style='background:#ACF9B8;color:#008888;'>";
     echo "<tr><th>Name</th><th>Location</th><th>Capacity</th><th>Available Date</th><th>Contact Details</th></tr> ";
